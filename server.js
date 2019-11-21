7a39900ff5100cc05d51636131e03de8a923d2d5
Imzan Khan <imzan@mindtrades.com>
2019-11-20 20:59:07 -0500

    Add comments to certain parts of code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
+//allows the use of folders i.e css, scripts etc.
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json({ extended: false }));
@@ -18,6 +19,7 @@ app.get("/public", function(req, res) {
 
 app.use("/api/seating", require("./routes/seating"));
 
+//for heroku hosting
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Connected on PORT: ${PORT}`));
